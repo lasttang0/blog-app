e7302af091e62f4da35cd29a40eac29b76134426
Evgeny Temchenko <temchenko.evgeny@gmail.com>
2023-08-19 18:16:21 +0600

    admin panel improvements

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
-from . import models
+from .models import Post, Category
 
-admin.site.register(models.Post)
-admin.site.register(models.Category)
+admin.site.site_header = 'Blog Admin'
+admin.site.site_title = 'Blog App'
+admin.site.index_title = 'Welcome to the Blog admin area'
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_on', 'last_modified')
+    search_fields = ('title', 'body')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'created_on')
--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,10 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=20)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Post(models.Model):
@@ -11,6 +15,9 @@ class Post(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     last_modified = models.DateTimeField(auto_now=True)
     categories = models.ManyToManyField('Category', related_name='posts')
+
+    def __str__(self):
+        return self.title
 
 
 class Comment(models.Model):
