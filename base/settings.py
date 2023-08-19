4b717811d1d426e60950b86d8372229e14a11939
Evgeny Temchenko <temchenko.evgeny@gmail.com>
2023-08-19 17:23:44 +0600

    add admin panel to navbar, add django debug toolbar

--- base/settings.py
+++ base/settings.py
@@ -27,6 +27,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+INTERNAL_IPS = [
+    # ...
+    "127.0.0.1",
+    # ...
+]
+
 
 # Application definition
 
@@ -37,10 +43,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'blog.apps.BlogConfig',
+    "blog.apps.BlogConfig",
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
--- base/urls.py
+++ base/urls.py
@@ -6,5 +6,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
-    path('blog/', include('blog.urls')),
+    path('__debug__/', include('debug_toolbar.urls')),
 ]
